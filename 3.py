c56162195d900646d45cce3966950b56d1a45848
Acami11 <camilahv28@gmail.com>
2026-04-24 20:06:19 -0500

    todo listo

--- 3.py
+++ 3.py
@@ -1,11 +1,11 @@
 
+
 def insertion_sort(A):
     for i in range(1, len(A)):
         j = i
         while j > 0 and A[j - 1] > A[j]:
-            A[j], A[j - 1] = A[j - 1], A[j]  # swap
+            A[j], A[j - 1] = A[j - 1], A[j]
             j -= 1
     return A
 
-lista = [5, 2, 4, 6, 1, 3]
-print (insertion_sort("lista"))
+print(insertion_sort([3,1,2,8,5,0,4])) 
